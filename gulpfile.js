4cdc047336d89b8dc392c2668fcedfc8ffd1d788
John Cyrill Corsanes <johncyrillcorsanes@gmail.com>
2019-02-23 12:29:11 +0800

    Another changes for Ruby, Gulp, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
+const argv = require('yargs').argv;
 const gulp = require('gulp')
+const shell = require('gulp-shell')
 const validator = require('gulp-html')
 
 gulp.task('html-check', () => {
   return gulp.src('index.html')
     .pipe(validator());
     // .pipe(gulp.dest('')); // default destination instead
+});
+
+// TODO: Magic compile
+gulp.task('compile-sass', () => {
+  console.log('This requires sass gem!');
+  return gulp.src('*.js', {read: false})
+  .pipe(shell([
+    'sass '+argv.i+' '+argv.o
+  ]));
 });
