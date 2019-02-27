57cb8549e8e8ca8a1017e6227f1326eedc9169d8
John Cyrill Corsanes <johncyrillcorsanes@gmail.com>
2019-02-27 20:23:17 +0800

    Disabled gulp tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,21 @@ const gulp = require("gulp");
 const shell = require("gulp-shell");
 const validator = require("gulp-html");
 
-gulp.task("homepage", () => {
-  return gulp.src("index.html").pipe(validator());
+gulp.task("homepage", (done) => {
+  // return gulp.src("index.html").pipe(validator());
+  done();
 });
 
-gulp.task("csass", () => {
-  return gulp
-    .src("*.js", {
-      read: false
-    })
-    .pipe(
-      shell([
-        "sass css/custom.scss css/custom.css && " +
-          "sass css/media.scss css/media.css"
-      ])
-    );
-});
+// DISABLED
+// gulp.task("csass", () => {
+//   return gulp
+//     .src("*.js", {
+//       read: false
+//     })
+//     .pipe(
+//       shell([
+//         "sass css/custom.scss css/custom.css && " +
+//           "sass css/media.scss css/media.css"
+//       ])
+//     );
+// });
