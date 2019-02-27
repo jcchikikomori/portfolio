add685aec9e295765f07d60ee7e18074e9f24926
John Cyrill Corsanes <johncyrillcorsanes@gmail.com>
2019-02-27 18:01:55 +0800

    Vue Deployment script

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/my-project/'
+//     : '/'
+// }
+
+// direct deploy
+module.exports = {
+  publicPath: '/'
+}
