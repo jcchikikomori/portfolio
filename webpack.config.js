f4c49a4ccb6232f0df660680297ba97f1ea85a67
John Cyrill Corsanes <jccorsanes@protonmail.com>
2020-12-12 13:19:07 +0800

    build(config, webpack): config cleanup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ["@vue/app"],
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
